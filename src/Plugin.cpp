5a79b3b6541f3a874eb2f2b2bb270c83ce6e5764
Patrix <patrixon9999@wp.pl>
2025-11-05 10:32:36 +0100

    Added `SIGNATURE_OF` support for Hooks

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -1,34 +1,36 @@
 // Disable macro redefinition warning
 #pragma warning(disable: 4005)
 
+#include "resource.h"
+
 #include <Union/Hook.h>
 #include <ZenGin/zGothicAPI.h>
 
 #ifdef __G1
 #define GOTHIC_NAMESPACE Gothic_I_Classic
 #define ENGINE Engine_G1
-HOOKSPACE(Gothic_I_Classic, GetGameVersion() == ENGINE);
+HOOKSPACE_WITH_SG_RC(Gothic_I_Classic, GetGameVersion() == ENGINE, IDR_GOTHIC_1_CLASSIC_TXT, RT_RCDATA);
 #include "Sources.hpp"
 #endif
 
 #ifdef __G1A
 #define GOTHIC_NAMESPACE Gothic_I_Addon
 #define ENGINE Engine_G1A
-HOOKSPACE(Gothic_I_Addon, GetGameVersion() == ENGINE);
+HOOKSPACE_WITH_SG_RC(Gothic_I_Addon, GetGameVersion() == ENGINE, IDR_GOTHIC_1_ADDON_TXT, RT_RCDATA);
 #include "Sources.hpp"
 #endif
 
 #ifdef __G2
 #define GOTHIC_NAMESPACE Gothic_II_Classic
 #define ENGINE Engine_G2
-HOOKSPACE(Gothic_II_Classic, GetGameVersion() == ENGINE);
+HOOKSPACE_WITH_SG_RC(Gothic_II_Classic, GetGameVersion() == ENGINE, IDR_GOTHIC_2_CLASSIC_TXT, RT_RCDATA);
 #include "Sources.hpp"
 #endif
 
 #ifdef __G2A
 #define GOTHIC_NAMESPACE Gothic_II_Addon
 #define ENGINE Engine_G2A
-HOOKSPACE(Gothic_II_Addon, GetGameVersion() == ENGINE);
+HOOKSPACE_WITH_SG_RC(Gothic_II_Addon, GetGameVersion() == ENGINE, IDR_GOTHIC_2_ADDON_TXT, RT_RCDATA);
 #include "Sources.hpp"
 #endif
 
